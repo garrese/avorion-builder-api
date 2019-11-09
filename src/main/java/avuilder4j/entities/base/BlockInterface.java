522feb5df61f4aba3d6c0e2a551aabe82ef56ec2
poiggi <49403460+poiggi@users.noreply.github.com>
2019-11-09 18:24:32 +0100

    class missed

---
+++ src/main/java/avuilder4j/entities/base/BlockInterface.java
@@ -0,0 +1,31 @@
+package avuilder4j.entities.base;
+
+public interface BlockInterface {
+
+	public Integer getIndex();
+
+	public Integer getParentIndex();
+
+	public Double getLX();
+
+	public Double getLY();
+
+	public Double getLZ();
+
+	public Double getUX();
+
+	public Double getUY();
+
+	public Double getUZ();
+
+	public Integer getMaterial();
+
+	public Integer getType();
+
+	public Integer getLook();
+
+	public Integer getUp();
+
+	public String getColor();
+
+}
